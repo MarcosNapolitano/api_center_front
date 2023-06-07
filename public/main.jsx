4e779b0975b06c82204d6a661772308ec42663f1
unknown <marcosnapolitano@hotmail.com.ar>
2023-06-07 20:02:38 -0300

    fix: html routing

--- public/main.jsx
+++ public/main.jsx
@@ -1,5 +1,5 @@
 import {createRoot} from "react-dom/client"
-import Main_Hub from "./App/Main_Hub.jsx"
+import Main_Hub from "../App/Main_Hub.jsx"
 
 const root = createRoot(document.getElementById('app'))
 
